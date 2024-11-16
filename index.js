244da9650cf2f30318b99ea556aea852084d9908
titusedwardgoh <titus.edward.goh@gmail.com>
2024-11-17 07:24:29 +1100

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,10 @@ menuArray.forEach((item)=>{
 
 function renderOrderedItems(){
     let totalPrice = 0
-   
+    orderedItems.forEach((item)=>{
+        totalPrice += item.price*item.count
+    })
+
     if (orderedItems.length===0 || totalPrice ===0 ){
         orderedItemsEl.classList.add("hidden")
         return
